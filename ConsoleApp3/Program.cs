c9fc96ba6f623ee3887f3de194b4f439068cd086
brkkalelii <mehmetmali331@gmail.com>
2021-10-06 21:57:32 +0300

    donguler

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,11 +4,36 @@ namespace ConsoleApp3
 {
     class Program
     {
+
+ 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = 0;
+            while (i < 10)
+            {
+                i = i + 1;
+                Console.WriteLine("While Döngüsü : " + i);
+            }
+            //var = degeri otomatik algiliyor
+            var k = 0;
 
-            Console.Read();
+            do
+            {
+                k++;
+                Console.WriteLine("do..while döngüsü: " + k);
+
+            } while (k < 10);
+
+
+            int toplam = 0;
+            for (int sayi = 1; sayi < 11; sayi++)
+            {
+                toplam = toplam + i;
+            }
+
+
+            }
+
         }
     }
-}
+
